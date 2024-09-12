1b1f5c60fb407dca3d7753357c98b4d53b922b1d
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-12 14:19:06 -0300

    atts

--- src/app/jogos/ciencias/acessorios/acessorios.component.ts
+++ src/app/jogos/ciencias/acessorios/acessorios.component.ts
@@ -6,6 +6,9 @@ import { InternacionalizacaoService } from '../../../main/internacionalizacao/in
 import { JogoService } from '../../linguagens/shared/service/jogo.service';
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ReacoesService } from '../../../componentes/shared/services/reacoes.service';
+import { TextToSpeechService } from '../../../componentes/shared/services/text-to-speech.service';
+import { PuzzleNome } from '../../linguagens/shared/model/puzzle-nome';
+import { DropNome } from '../../linguagens/shared/model/drop-nome';
 
 @Component({
   selector: 'app-acessorios',
@@ -13,18 +16,9 @@ import { ReacoesService } from '../../../componentes/shared/services/reacoes.ser
   styleUrls: ['./acessorios.component.css','../shared/style.css']
 })
 export class AcessoriosComponent {
-  public objetos: Puzzle[] = [
-    { imagem: '../../../assets/ciencias/acessorios/1.png', posicaoCorreta: 6 },
-    { imagem: '../../../assets/ciencias/acessorios/2.png', posicaoCorreta: 0 },
-    { imagem: '../../../assets/ciencias/acessorios/3.png', posicaoCorreta: 2 },
-    { imagem: '../../../assets/ciencias/acessorios/4.png', posicaoCorreta: 3 },
-    { imagem: '../../../assets/ciencias/acessorios/5.png', posicaoCorreta: 4 },
-    { imagem: '../../../assets/ciencias/acessorios/6.png', posicaoCorreta: 7 },
-    { imagem: '../../../assets/ciencias/acessorios/7.png', posicaoCorreta: 1 },
-    { imagem: '../../../assets/ciencias/acessorios/8.png', posicaoCorreta: 5 }
-  ];
+  public objetos: PuzzleNome[] = [];
   public posicoesCorretasMeiaESapato: number[] = [6,7];
-  public listaDrop: Drop[] = [];
+  public listaDrop: DropNome[] = [];
   public x: string[] = ['17px','327px'];
   public y: string[] = ['58px','133px','210px','286px'];
   public acertou: boolean[] = [false, false, false, false, false, false, false, false];
@@ -34,28 +28,41 @@ export class AcessoriosComponent {
   constructor(
     private embaralhaListaService: EmbaralharListaService,
     private interService: InternacionalizacaoService,
-    private jogoService: JogoService) { }
+    private jogoService: JogoService,
+    private ttsService: TextToSpeechService
+  ) { }
 
   ngOnInit() {
-    this.carregarDrops();
     this.literals = this.interService.getIdioma();
+    this.carregarDrops();
+    this.ttsService.lerTexto(this.literals.jogoAcessoriosDescricao + '. ' + this.literals.jogoAcessoriosDica)
   }
 
   public carregarDrops() {
+    this.objetos = [
+      { imagem: '../../../assets/ciencias/acessorios/1.png', posicaoCorreta: 6, nome: this.literals.meias },
+      { imagem: '../../../assets/ciencias/acessorios/2.png', posicaoCorreta: 0, nome: this.literals.laco },
+      { imagem: '../../../assets/ciencias/acessorios/3.png', posicaoCorreta: 2, nome: this.literals.oculos },
+      { imagem: '../../../assets/ciencias/acessorios/4.png', posicaoCorreta: 3, nome: this.literals.anel },
+      { imagem: '../../../assets/ciencias/acessorios/5.png', posicaoCorreta: 4, nome: this.literals.saia },
+      { imagem: '../../../assets/ciencias/acessorios/6.png', posicaoCorreta: 7, nome: this.literals.sapatilha },
+      { imagem: '../../../assets/ciencias/acessorios/7.png', posicaoCorreta: 1, nome: this.literals.brincos },
+      { imagem: '../../../assets/ciencias/acessorios/8.png', posicaoCorreta: 5, nome: this.literals.blusa }
+    ];
     this.embaralhaListaService.embaralhaLista(this.objetos);
     this.listaDrop = [
-      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y[0], puzzleList: []},
-      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y[0], puzzleList: []},
-      { posicao: 2, posicaoX: this.x[0], posicaoY: this.y[1], puzzleList: []},
-      { posicao: 3, posicaoX: this.x[1], posicaoY: this.y[1], puzzleList: []},
-      { posicao: 4, posicaoX: this.x[0], posicaoY: this.y[2], puzzleList: []},
-      { posicao: 5, posicaoX: this.x[1], posicaoY: this.y[2], puzzleList: []},
-      { posicao: 6, posicaoX: this.x[0], posicaoY: this.y[3], puzzleList: []},
-      { posicao: 7, posicaoX: this.x[1], posicaoY: this.y[3], puzzleList: []}
+      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y[0], puzzleList: [], nome: this.literals.cabelos},
+      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y[0], puzzleList: [], nome: this.literals.orelhas},
+      { posicao: 2, posicaoX: this.x[0], posicaoY: this.y[1], puzzleList: [], nome: this.literals.olhos},
+      { posicao: 3, posicaoX: this.x[1], posicaoY: this.y[1], puzzleList: [], nome: this.literals.maos},
+      { posicao: 4, posicaoX: this.x[0], posicaoY: this.y[2], puzzleList: [], nome: this.literals.pernas},
+      { posicao: 5, posicaoX: this.x[1], posicaoY: this.y[2], puzzleList: [], nome: this.literals.tronco},
+      { posicao: 6, posicaoX: this.x[0], posicaoY: this.y[3], puzzleList: [], nome: this.literals.pes},
+      { posicao: 7, posicaoX: this.x[1], posicaoY: this.y[3], puzzleList: [], nome: this.literals.pes}
     ];
   }
 
-  public drop(event: CdkDragDrop<Puzzle[]>) {
+  public drop(event: CdkDragDrop<PuzzleNome[]>) {
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -64,7 +71,7 @@ export class AcessoriosComponent {
     );
   }
 
-  public dropResposta(event: CdkDragDrop<Puzzle[]>, drop: Drop) {
+  public dropResposta(event: CdkDragDrop<PuzzleNome[]>, drop: DropNome) {
     if(drop.puzzleList.length > 0) {
       return;
     }
@@ -77,7 +84,7 @@ export class AcessoriosComponent {
       this.verificaRespostas(drop);
   }
 
-  public verificaRespostas(drop: Drop): void {
+  public verificaRespostas(drop: DropNome): void {
     if(drop.posicao === 6 || drop.posicao === 7) {
       if(drop.puzzleList[0].posicaoCorreta == this.posicoesCorretasMeiaESapato[0] || drop.puzzleList[0].posicaoCorreta == this.posicoesCorretasMeiaESapato[1]) {
         this.acertou[drop.posicao] = true;
@@ -95,12 +102,23 @@ export class AcessoriosComponent {
     } else {
       this.acertou[drop.posicao] = false;
       ReacoesService.mudarReacao.emit('triste');
-        this.jogoService.tirarPontos();
+      this.jogoService.tirarPontos();
     }
     if(this.acertou[0] && this.acertou[1] && this.acertou[2] && this.acertou[3] && this.acertou[4] && this.acertou[5] && this.acertou[6] && this.acertou[7]) {
       this.acertouTudo = true;
       ReacoesService.mudarReacao.emit('acertou tudo');
       this.jogoService.adicionarNivel();
     }
+  }
+
+  public lerTexto(texto: string): void {
+    this.ttsService.pararLeitura();
+    setTimeout(() => {
+      this.ttsService.lerTexto(texto);
+    }, 200);
+  }
+
+  public parar(): void {
+    this.ttsService.pararLeitura();
   }
 }
