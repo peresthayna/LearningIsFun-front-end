29abbd365bd327733bc38c273fa42eef3aa0ee52
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-12 16:04:27 -0300

    atts

--- src/app/jogos/linguagens/cores-frutas/cores-frutas.component.ts
+++ src/app/jogos/linguagens/cores-frutas/cores-frutas.component.ts
@@ -6,6 +6,9 @@ import { EmbaralharListaService } from '../shared/service/embaralha-lista.servic
 import { InternacionalizacaoService } from '../../../main/internacionalizacao/internacionalizacao.service';
 import { JogoService } from '../shared/service/jogo.service';
 import { ReacoesService } from '../../../componentes/shared/services/reacoes.service';
+import { PuzzleNome } from '../shared/model/puzzle-nome';
+import { DropNome } from '../shared/model/drop-nome';
+import { TextToSpeechService } from '../../../componentes/shared/services/text-to-speech.service';
 
 @Component({
   selector: 'app-cores-frutas',
@@ -15,13 +18,8 @@ import { ReacoesService } from '../../../componentes/shared/services/reacoes.ser
 export class CoresFrutasComponent implements OnInit {
 
   public literals: any;
-  public formas: Puzzle[] = [
-    { imagem: '../../../assets/lp/frutas/amarelo.png', posicaoCorreta: 0 },
-    { imagem: '../../../assets/lp/frutas/roxo.png', posicaoCorreta: 1 },
-    { imagem: '../../../assets/lp/frutas/vermelho.png', posicaoCorreta: 2 },
-    { imagem: '../../../assets/lp/frutas/verde.png', posicaoCorreta: 3 }
-  ];
-  public listaDrop: Drop[] = [];
+  public frutas: PuzzleNome[] = [];
+  public listaDrop: DropNome[] = [];
   public x: string[] = ['-125px','25px','175px','325px'];
   public y: string = '50px';
   public acertou: boolean[] = [false, false, false, false];
@@ -30,25 +28,33 @@ export class CoresFrutasComponent implements OnInit {
   constructor(
     private interService: InternacionalizacaoService,
     private embaralharListaService: EmbaralharListaService,
-    private jogoService: JogoService
+    private jogoService: JogoService,
+    private ttsService: TextToSpeechService
   ) { }
 
   ngOnInit() {
     this.literals = this.interService.getIdioma();
     this.carregarDrops();
+    this.lerTexto(this.literals.jogoCoresFrutasDescricao + '. ' + this.literals.jogoCoresFrutasDica);
   }
 
   public carregarDrops() {
-    this.embaralharListaService.embaralhaLista(this.formas);
+    this.frutas = [
+      { imagem: '../../../assets/lp/frutas/amarelo.png', posicaoCorreta: 0, nome: this.literals.amarelo },
+      { imagem: '../../../assets/lp/frutas/roxo.png', posicaoCorreta: 1, nome: this.literals.roxo },
+      { imagem: '../../../assets/lp/frutas/vermelho.png', posicaoCorreta: 2, nome: this.literals.vermelho },
+      { imagem: '../../../assets/lp/frutas/verde.png', posicaoCorreta: 3, nome: this.literals.verde }
+    ];
+    this.embaralharListaService.embaralhaLista(this.frutas);
     this.listaDrop = [
-      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y, puzzleList: []},
-      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y, puzzleList: []},
-      { posicao: 2, posicaoX: this.x[2], posicaoY: this.y, puzzleList: []},
-      { posicao: 3, posicaoX: this.x[3], posicaoY: this.y, puzzleList: []}
+      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y, puzzleList: [], nome: this.literals.banana },
+      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y, puzzleList: [], nome: this.literals.uva },
+      { posicao: 2, posicaoX: this.x[2], posicaoY: this.y, puzzleList: [], nome: this.literals.morango },
+      { posicao: 3, posicaoX: this.x[3], posicaoY: this.y, puzzleList: [], nome: this.literals.pera }
     ];
   }
 
-  public drop(event: CdkDragDrop<Puzzle[]>) {
+  public drop(event: CdkDragDrop<PuzzleNome[]>) {
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -57,7 +63,7 @@ export class CoresFrutasComponent implements OnInit {
     );
   }
 
-  public dropResposta(event: CdkDragDrop<Puzzle[]>, drop: Drop) {
+  public dropResposta(event: CdkDragDrop<PuzzleNome[]>, drop: DropNome) {
     if(drop.puzzleList.length > 0) {
       return;
     }
@@ -70,7 +76,7 @@ export class CoresFrutasComponent implements OnInit {
     this.verificaRespostas(drop);
   }
 
-  public verificaRespostas(drop: Drop): void {
+  public verificaRespostas(drop: DropNome): void {
     if(drop.puzzleList[0].posicaoCorreta == drop.posicao) {
       this.acertou[drop.posicao] = true;
       this.jogoService.adicionarPontos();
@@ -85,6 +91,17 @@ export class CoresFrutasComponent implements OnInit {
       this.jogoService.adicionarNivel();
       ReacoesService.mudarReacao.emit('acertou tudo');
     }
+  }
+
+  public lerTexto(texto: string): void {
+    this.ttsService.pararLeitura();
+    setTimeout(() => {
+      this.ttsService.lerTexto(texto);
+    }, 200);
+  }
+
+  public parar(): void {
+    this.ttsService.pararLeitura();
   }
 
 }
