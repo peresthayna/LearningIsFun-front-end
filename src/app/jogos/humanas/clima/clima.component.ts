4ea02f06ecc42b8f5fe7f355d991493508341827
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-12 15:09:59 -0300

    atts

--- src/app/jogos/humanas/clima/clima.component.ts
+++ src/app/jogos/humanas/clima/clima.component.ts
@@ -5,15 +5,17 @@ import { InternacionalizacaoService } from '../../../main/internacionalizacao/in
 import { JogoService } from '../../linguagens/shared/service/jogo.service';
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ReacoesService } from '../../../componentes/shared/services/reacoes.service';
+import { PuzzleNome } from '../../linguagens/shared/model/puzzle-nome';
+import { TextToSpeechService } from '../../../componentes/shared/services/text-to-speech.service';
 
 export class Coluna {
   literals: string;
-  puzzleItems: Puzzle[];
+  puzzleItems: PuzzleNome[];
   posicao: number;
   class: string;
   img: string;
 
-  constructor(literals: string, puzzleItems: Puzzle[], posicao: number, classCss: string, img: string) {
+  constructor(literals: string, puzzleItems: PuzzleNome[], posicao: number, classCss: string, img: string) {
     this.literals = literals;
     this.puzzleItems = puzzleItems;
     this.posicao = posicao;
@@ -28,22 +30,8 @@ export class Coluna {
   styleUrls: ['./clima.component.css', '../shared/style.css']
 })
 export class ClimaComponent {
-  public puzzleItems: Puzzle[] = [
-    { imagem: '../../../assets/humanas/clima/biquini.png', posicaoCorreta: 0 }, // Sol
-    { imagem: '../../../assets/humanas/clima/bone.png', posicaoCorreta: 0 }, // Sol
-    { imagem: '../../../assets/humanas/clima/capadechuva.png', posicaoCorreta: 1 }, // Chuva
-    { imagem: '../../../assets/humanas/clima/casaco.png', posicaoCorreta: 1 }, // Chuva
-    { imagem: '../../../assets/humanas/clima/chinelo.png', posicaoCorreta: 0 }, // Sol
-    { imagem: '../../../assets/humanas/clima/galochas.png', posicaoCorreta: 1 }, // Chuva
-    { imagem: '../../../assets/humanas/clima/guardachuva.png', posicaoCorreta: 1 }, // Chuva
-    { imagem: '../../../assets/humanas/clima/oculosdesol.png', posicaoCorreta: 0 } // Sol
-  ];
-
-  public colunas: Coluna[] = [
-    new Coluna('sol', [], 0, 'drop-place-sun', '../../../assets/humanas/clima/sol.jpg'),
-    new Coluna('chuva', [], 1, 'drop-place-rain', '../../../assets/humanas/clima/chuva.png'),
-    new Coluna('objetos', this.puzzleItems, -1, 'drop-place-objects', '')
-  ];
+  public puzzleItems: PuzzleNome[] = [];
+  public colunas: Coluna[] = [];
   public literals: any;
   public acertou: boolean[] = [false, false, false, false, false, false, false, false];
   public acertouTudo: boolean = false;
@@ -51,16 +39,37 @@ export class ClimaComponent {
   constructor(
     private embaralhaListaService: EmbaralharListaService,
     private interService: InternacionalizacaoService,
-    private jogoService: JogoService
+    private jogoService: JogoService,
+    private ttsService: TextToSpeechService
   ) { }
 
   ngOnInit() {
     this.literals = this.interService.getIdioma();
+    this.lerTexto(this.literals.jogoClimaDescricao + '. ' + this.literals.jogoClimaDica);
+    this.carregarColunas();
     this.embaralhaListaService.embaralhaLista(this.puzzleItems);
   }
 
+  public carregarColunas(): void {
+    this.puzzleItems = [
+      { imagem: '../../../assets/humanas/clima/biquini.png', posicaoCorreta: 0, nome: this.literals.biquini }, // Sol
+      { imagem: '../../../assets/humanas/clima/bone.png', posicaoCorreta: 0, nome: this.literals.bone }, // Sol
+      { imagem: '../../../assets/humanas/clima/capadechuva.png', posicaoCorreta: 1, nome: this.literals.capaChuva }, // Chuva
+      { imagem: '../../../assets/humanas/clima/casaco.png', posicaoCorreta: 1, nome: this.literals.casaco }, // Chuva
+      { imagem: '../../../assets/humanas/clima/chinelo.png', posicaoCorreta: 0, nome: this.literals.chinelo }, // Sol
+      { imagem: '../../../assets/humanas/clima/galochas.png', posicaoCorreta: 1, nome: this.literals.galochas }, // Chuva
+      { imagem: '../../../assets/humanas/clima/guardachuva.png', posicaoCorreta: 1, nome: this.literals.guardaChuva }, // Chuva
+      { imagem: '../../../assets/humanas/clima/oculosdesol.png', posicaoCorreta: 0, nome: this.literals.oculosSol } // Sol
+    ];
+    this.colunas = [
+      new Coluna(this.literals.sol, [], 0, 'drop-place-sun', '../../../assets/humanas/clima/sol.jpg'),
+      new Coluna(this.literals.chuva, [], 1, 'drop-place-rain', '../../../assets/humanas/clima/chuva.png'),
+      new Coluna(this.literals.objetos, this.puzzleItems, -1, 'drop-place-objects', '')
+    ];
+  }
 
-  public drop(event: CdkDragDrop<Puzzle[]>, coluna: Coluna) {
+
+  public drop(event: CdkDragDrop<PuzzleNome[]>, coluna: Coluna) {
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -71,7 +80,7 @@ export class ClimaComponent {
     this.verificarResposta(event, coluna);
   }
 
-  public verificarResposta(event: CdkDragDrop<Puzzle[]>, coluna: Coluna) {
+  public verificarResposta(event: CdkDragDrop<PuzzleNome[]>, coluna: Coluna) {
     if(coluna.posicao === -1
       || event.container === event.previousContainer) {
         return;
@@ -91,5 +100,16 @@ export class ClimaComponent {
       ReacoesService.mudarReacao.emit('acertou tudo');
       this.jogoService.adicionarNivel();
     }
+  }
+
+  public lerTexto(texto: string): void {
+    this.ttsService.pararLeitura();
+    setTimeout(() => {
+      this.ttsService.lerTexto(texto);
+    }, 200);
+  }
+
+  public parar(): void {
+    this.ttsService.pararLeitura();
   }
 }
