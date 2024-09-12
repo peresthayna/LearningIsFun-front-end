d7aee79f8ba0d8b9f2f85a55299701fb4d6067a8
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-12 16:37:14 -0300

    atts

--- src/app/jogos/matematica/encaixar-formas/encaixar-formas.component.ts
+++ src/app/jogos/matematica/encaixar-formas/encaixar-formas.component.ts
@@ -6,6 +6,9 @@ import { EmbaralharListaService } from '../../linguagens/shared/service/embaralh
 import { JogoService } from '../../linguagens/shared/service/jogo.service';
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ReacoesService } from '../../../componentes/shared/services/reacoes.service';
+import { TextToSpeechService } from '../../../componentes/shared/services/text-to-speech.service';
+import { PuzzleNome } from '../../linguagens/shared/model/puzzle-nome';
+import { DropNome } from '../../linguagens/shared/model/drop-nome';
 
 @Component({
   selector: 'app-encaixar-formas',
@@ -14,13 +17,8 @@ import { ReacoesService } from '../../../componentes/shared/services/reacoes.ser
 })
 export class EncaixarFormasComponent {
   public literals: any;
-  public formas: Puzzle[] = [
-    { imagem: '../../../assets/matematica/encaixar-formas/circulo.png', posicaoCorreta: 1 },
-    { imagem: '../../../assets/matematica/encaixar-formas/quadrado.png', posicaoCorreta: 2 },
-    { imagem: '../../../assets/matematica/encaixar-formas/retangulo.png', posicaoCorreta: 3 },
-    { imagem: '../../../assets/matematica/encaixar-formas/triangulo.png', posicaoCorreta: 0 }
-  ];
-  public listaDrop: Drop[] = [];
+  public formas: PuzzleNome[] = [];
+  public listaDrop: DropNome[] = [];
   public x: string[] = ['-110px','40px','190px','340px'];
   public y: string = '55px';
   public acertou: boolean[] = [false, false, false, false];
@@ -29,25 +27,33 @@ export class EncaixarFormasComponent {
   constructor(
     private interService: InternacionalizacaoService,
     private embaralharListaService: EmbaralharListaService,
-    private jogoService: JogoService
+    private jogoService: JogoService,
+    private ttsService: TextToSpeechService
   ) { }
 
   ngOnInit() {
     this.literals = this.interService.getIdioma();
     this.carregarDrops();
+    this.lerTexto(this.literals.jogoEncaixarFormasDescricao + '. ' + this.literals.jogoEncaixarFormasDica);
   }
 
   public carregarDrops() {
+    this.formas = [
+      { imagem: '../../../assets/matematica/encaixar-formas/circulo.png', posicaoCorreta: 1, nome: this.literals.circulo },
+      { imagem: '../../../assets/matematica/encaixar-formas/quadrado.png', posicaoCorreta: 2, nome: this.literals.quadrado },
+      { imagem: '../../../assets/matematica/encaixar-formas/retangulo.png', posicaoCorreta: 3, nome: this.literals.retangulo },
+      { imagem: '../../../assets/matematica/encaixar-formas/triangulo.png', posicaoCorreta: 0, nome: this.literals.triangulo }
+    ];
     this.embaralharListaService.embaralhaLista(this.formas);
     this.listaDrop = [
-      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y, puzzleList: []},
-      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y, puzzleList: []},
-      { posicao: 2, posicaoX: this.x[2], posicaoY: this.y, puzzleList: []},
-      { posicao: 3, posicaoX: this.x[3], posicaoY: this.y, puzzleList: []}
+      { posicao: 0, posicaoX: this.x[0], posicaoY: this.y, puzzleList: [], nome: this.literals.triangulo},
+      { posicao: 1, posicaoX: this.x[1], posicaoY: this.y, puzzleList: [], nome: this.literals.quadrado},
+      { posicao: 2, posicaoX: this.x[2], posicaoY: this.y, puzzleList: [], nome: this.literals.retangulo},
+      { posicao: 3, posicaoX: this.x[3], posicaoY: this.y, puzzleList: [], nome: this.literals.triangulo }
     ];
   }
 
-  public drop(event: CdkDragDrop<Puzzle[]>) {
+  public drop(event: CdkDragDrop<PuzzleNome[]>) {
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -56,7 +62,7 @@ export class EncaixarFormasComponent {
     );
   }
 
-  public dropResposta(event: CdkDragDrop<Puzzle[]>, drop: Drop) {
+  public dropResposta(event: CdkDragDrop<PuzzleNome[]>, drop: DropNome) {
     if(drop.puzzleList.length > 0) {
       return;
     }
@@ -69,7 +75,7 @@ export class EncaixarFormasComponent {
     this.verificaRespostas(drop);
   }
 
-  public verificaRespostas(drop: Drop): void {
+  public verificaRespostas(drop: DropNome): void {
     if(drop.puzzleList[0].posicaoCorreta == drop.posicao) {
       this.acertou[drop.posicao] = true;
       ReacoesService.mudarReacao.emit('acertou');
@@ -84,5 +90,16 @@ export class EncaixarFormasComponent {
       ReacoesService.mudarReacao.emit('acertou tudo');
       this.jogoService.adicionarNivel();
     }
+  }
+
+  public lerTexto(texto: string): void {
+    this.ttsService.pararLeitura();
+    setTimeout(() => {
+      this.ttsService.lerTexto(texto);
+    }, 200);
+  }
+
+  public parar(): void {
+    this.ttsService.pararLeitura();
   }
 }
